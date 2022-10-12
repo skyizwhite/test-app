aa5e17882d7ae06243b6a99ccc0f8b49f61dad9b
paku <paku@skyizwhite.dev>
2022-10-12 15:39:42 +0900

    Fix error

--- app.lisp
+++ app.lisp
@@ -1,7 +1,3 @@
-(ql:quickload "markup")
-(markup:enable-reader)
-
 (lambda (env)
   (declare (ignore env))
-  `(200 (:content-type "text/html")
-        (,(markup:write-html <h1>Hello World from Common Lisp!</h1>))))
+  `(200 (:content-type "text/html") ("<h1>Hello World!</h1>")))
